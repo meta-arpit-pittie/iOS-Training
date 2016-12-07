fc25a52c271e1167649c8217d41c86145c84135e
meta-arpit-pittie <arpit.pittie@metacube.com>
2016-12-07 12:03:26 +0530

    Implemented the Table View
    Table View, Segue, Persist Data

---
+++ FoodTracker/FoodTracker/Meal.swift
@@ -0,0 +1,66 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Metacube on 05/12/16.
+//  Copyright © 2016 Metacube. All rights reserved.
+//
+
+import UIKit
+
+class Meal: NSObject, NSCoding {
+    // MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    // MARK: Archiving Paths
+    
+    //static var documentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    //static let archiveURL = documentsDirectory.appendPathComponent("meals")
+    
+    static let documentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let archiveURL = documentsDirectory.appendingPathComponent("meals")
+    
+    // MARK: Types
+    
+    struct PropertyKey {
+        static let nameKey = "name"
+        static let photoKey = "photo"
+        static let ratingKey = "rating"
+    }
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+        super.init()
+        
+        // Initialization should fail if there is no name or if the rating is negative.
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
+    }
+    
+    // MARK: NSCoding
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: PropertyKey.nameKey)
+        aCoder.encode(photo, forKey: PropertyKey.photoKey)
+        aCoder.encode(rating, forKey: PropertyKey.ratingKey)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        let name = aDecoder.decodeObject(forKey: PropertyKey.nameKey) as! String
+        
+        // Because photo is an optional property of Meal, use conditional cast.
+        let photo = aDecoder.decodeObject(forKey: PropertyKey.photoKey) as? UIImage
+        
+        let rating = aDecoder.decodeInteger(forKey: PropertyKey.ratingKey)
+        
+        // Must call designated initializer.
+        self.init(name: name, photo: photo, rating: rating)
+    }
+}
