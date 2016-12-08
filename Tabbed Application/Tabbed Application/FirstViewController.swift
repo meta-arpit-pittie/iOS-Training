a95852336d45fd73b7790cb8136048972d0fe140
meta-arpit-pittie <arpit.pittie@metacube.com>
2016-12-08 10:00:19 +0530

    Tab View and Scroll View
    Scroll View not working

---
+++ Tabbed Application/Tabbed Application/FirstViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FirstViewController.swift
+//  Tabbed Application
+//
+//  Created by Arpit Pittie on 07/12/16.
+//  Copyright © 2016 Metacube. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    @IBOutlet weak var scrollViewForm: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+}
+
---
+++ Tabbed Application/Tabbed Application/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  Tabbed Application
+//
+//  Created by Arpit Pittie on 07/12/16.
+//  Copyright © 2016 Metacube. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
