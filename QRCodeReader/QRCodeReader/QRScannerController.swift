5c593e983b16ec4d5d7320736339588b36b06095
meta-arpit-pittie <arpit.pittie@metacube.com>
2017-01-11 18:38:25 +0530

    Bar Code Scanner

---
+++ QRCodeReader/QRCodeReader/QRScannerController.swift
@@ -0,0 +1,92 @@
+//
+//  QRScannerController.swift
+//  QRCodeReader
+//
+//  Created by Arpit Pittie on 11/01/17.
+//  Copyright © 2016 Metacube. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class QRScannerController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+
+    @IBOutlet var messageLabel:UILabel!
+    @IBOutlet var topbar: UIView!
+    
+    var captureSession:AVCaptureSession?
+    var videoPreviewLayer:AVCaptureVideoPreviewLayer?
+    var qrCodeFrameView:UIView?
+    
+    let supportedCodeTypes = [AVMetadataObjectTypeUPCECode,
+                              AVMetadataObjectTypeCode39Code,
+                              AVMetadataObjectTypeCode39Mod43Code,
+                              AVMetadataObjectTypeCode93Code,
+                              AVMetadataObjectTypeCode128Code,
+                              AVMetadataObjectTypeEAN8Code,
+                              AVMetadataObjectTypeEAN13Code,
+                              AVMetadataObjectTypeAztecCode,
+                              AVMetadataObjectTypePDF417Code,
+                              AVMetadataObjectTypeQRCode]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let captureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            let captureMetadataOutput = AVCaptureMetadataOutput()
+            
+            captureSession = AVCaptureSession()
+            
+            captureSession?.addInput(input)
+            captureSession?.addOutput(captureMetadataOutput)
+            
+            captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            captureMetadataOutput.metadataObjectTypes = supportedCodeTypes
+            
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+            videoPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+            videoPreviewLayer?.frame = view.layer.bounds
+            view.layer.addSublayer(videoPreviewLayer!)
+            
+            captureSession?.startRunning()
+            
+            view.bringSubview(toFront: messageLabel)
+            view.bringSubview(toFront: topbar)
+            
+            qrCodeFrameView = UIView()
+            
+            if let qrCodeFrameView = qrCodeFrameView {
+                qrCodeFrameView.layer.borderColor = UIColor.green.cgColor
+                qrCodeFrameView.layer.borderWidth = 2
+                view.addSubview(qrCodeFrameView)
+                view.bringSubview(toFront: qrCodeFrameView)
+            }
+        } catch {
+            print(error)
+        }
+    }
+    
+    // MARK: AVCaptureMetadataOutputObjectsDelegate
+    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [Any]!, from connection: AVCaptureConnection!) {
+        if metadataObjects == nil || metadataObjects.count == 0 {
+            qrCodeFrameView?.frame = CGRect.zero
+            messageLabel.text = "No QR code is detected"
+            return
+        }
+        
+        let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+        
+        if supportedCodeTypes.contains(metadataObj.type) {
+            print("I am here")
+            let barCodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObj)
+            qrCodeFrameView?.frame = barCodeObject!.bounds
+            
+            if metadataObj.stringValue != nil {
+                messageLabel.text = metadataObj.stringValue
+            }
+        }
+    }
+}
