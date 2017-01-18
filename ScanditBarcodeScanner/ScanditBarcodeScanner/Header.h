62d4a1cecfecebe3b7fd345d3283ca633d91811b
meta-arpit-pittie <arpit.pittie@metacube.com>
2017-01-18 17:04:50 +0530

    Scandit Work with Framework File

---
+++ ScanditBarcodeScanner/ScanditBarcodeScanner/Header.h
@@ -0,0 +1,9 @@
+//
+//  Header.h
+//  ScanditBarcodeScanner
+//
+//  Created by Arpit Pittie on 18/01/17.
+//  Copyright © 2017 Metacube. All rights reserved.
+//
+
+#import <ScanditBarcodeScanner/ScanditBarcodeScanner.h>
